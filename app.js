b521a123a9f8c87c52300c758fbc585d53683214
Paresh Bharambe <pareshb@live.in>
2017-12-29 14:23:37 +0530

    starting commit

---
+++ app.js
@@ -0,0 +1,29 @@
+console.log('Starting App');
+const fs = require('fs');
+const yargs = require('yargs');
+const Note = require('./modules/notes.js');
+
+var argsv = yargs.argv;
+
+var command = process.argv[2];
+if(command === undefined){
+	console.log('No command');
+}
+if(command === 'add'){
+	var newNote  = new Note(argsv.title, argsv.description);
+	newNote.add();
+} else if(command === 'read'){
+	var newNote = new Note(argsv.title);
+	newNote.read();
+} else if(command === 'delete'){
+	var newNote = new Note(argsv.title);
+	newNote.delete();
+} else if(command === 'list'){
+	notes = new Note();
+	notes.listAll();
+} else {
+	if(command){
+		console.log('command not recognized');
+	}
+}
+
