01b78e94a0fa005006169f02c11567d921d83981
Paresh Bharambe <pareshb@live.in>
2018-01-01 14:47:43 +0530

    Fixed month display

--- app.js
+++ app.js
@@ -27,5 +27,5 @@ if(command === 'add'){
 	}
 }
 let d = new Date();
-console.log(d.getDate() +"-"+ d.getMonth() + "-" + d.getFullYear());
+console.log(d.getDate() +"-"+ (d.getMonth()+1) + "-" + d.getFullYear());
 console.log('Happy New Year '+d.getFullYear());
